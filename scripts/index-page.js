c4cbb208b2e5f084f019c977ddc968bd21c0d13e
Zanab <zanab@protonmail.com>
2024-08-06 02:57:11 -0400

    Adding opentype to font styles

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -68,7 +68,7 @@ function createCommentDOMElement(comment) {
     commentTextDiv.append(commentText);
 
     commentName.innerText = comment.name;
-    commentTimestamp.innerText = timeSince(new Date(comment.timestamp));
+    commentTimestamp.innerText = new Date(comment.timestamp).toDateString();
 
     return commentCardContainerDiv;
 }
@@ -104,33 +104,5 @@ function printComments() {
     }
     commentsArray.reverse();
 }
-
-function timeSince(date) {
-
-    var seconds = Math.floor((new Date() - date) / 1000);
-  
-    var interval = seconds / 31536000;
-  
-    if (interval > 1) {
-      return Math.floor(interval) + " years ago";
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-      return Math.floor(interval) + " months ago";
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-      return Math.floor(interval) + " days ago";
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-      return Math.floor(interval) + " hours ago";
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-      return Math.floor(interval) + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-  }
 
 printComments();
