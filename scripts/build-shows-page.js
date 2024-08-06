30aec16f4d28f915ba2ea93de8fba82396fdfef6
Zanab <zanab@protonmail.com>
2024-08-06 00:25:10 -0400

    finished shows page

---
+++ scripts/build-shows-page.js
@@ -0,0 +1,157 @@
+// each show: date, venue, location
+// array of these shows
+const showList = [
+    {
+        date: '01/10/1994',
+        venue: 'islamabad',
+        location: 'Pakistan'
+    },
+    {
+        date: '01/10/1994',
+        venue: 'islamabad',
+        location: 'Pakistan'
+    },
+    {
+        date: '01/10/1994',
+        venue: 'islamabad',
+        location: 'Pakistan'
+    },
+    {
+        date: '01/10/1994',
+        venue: 'islamabad',
+        location: 'Pakistan'
+    },
+    {
+        date: '01/10/1994',
+        venue: 'islamabad',
+        location: 'Pakistan'
+    }
+];
+
+function buildShowsSection() {
+    const showsContent = document.createElement('div');
+    showsContent.classList.add('shows__content');
+    
+    const showsHeaderContainer = document.createElement('div');
+    showsHeaderContainer.classList.add('shows__header-container');
+    showsContent.append(showsHeaderContainer);
+
+    const showsHeader = document.createElement('h2');
+    const showTableHeader = document.createElement('div');
+    showTableHeader.classList.add('show-table-header');
+    showsHeader.innerText = 'Shows'
+    showsHeaderContainer.append(showsHeader);
+    showsHeaderContainer.append(showTableHeader);
+
+    const dateLabel = document.createElement('p');
+    const venueLabel = document.createElement('p');
+    const locationLabel = document.createElement('p');
+    const emptyLabel =  document.createElement('div');
+
+    dateLabel.classList.add('show-label');
+    dateLabel.innerText = 'DATE';
+
+    venueLabel.classList.add('show-label');
+    venueLabel.innerText = 'VENUE';
+
+    locationLabel.classList.add('show-label');
+    locationLabel.innerText = 'LOCATION';
+
+    showTableHeader.append(dateLabel);
+    showTableHeader.append(venueLabel);
+    showTableHeader.append(locationLabel);
+    showTableHeader.append(emptyLabel);
+
+    const showsListContainer = document.createElement('ul');
+    showsListContainer.classList.add('shows-container');
+
+    showsContent.append(showsListContainer);
+
+    const showsContainer = document.querySelector('.shows__container .section__content');
+    console.log(showsContainer);
+    showsContainer.append(showsContent);
+}
+
+
+function printShowTimes() {
+    const showsContainerDiv = document.getElementsByClassName('shows-container')[0];
+
+    showList.forEach(show => {
+        const showElement = createShowDOMElement(show);
+        showsContainerDiv.append(showElement);
+    });
+}
+
+function createShowDOMElement(show) {
+    const showItemDiv = document.createElement('li');
+    showItemDiv.classList = 'show-item';
+
+    const showDateDiv = document.createElement('div');
+    const showDateLabel = document.createElement('p');
+    showDateLabel.classList = 'show-label';
+    showDateLabel.innerText = 'DATE';
+    
+
+    const showDateValue = document.createElement('p');
+    showDateValue.classList = 'show-value show-date';
+    showDateValue.innerText = new Date(show.date).toDateString();
+
+    showDateDiv.append(showDateLabel);
+    showDateDiv.append(showDateValue);
+
+    const showVenueDiv = document.createElement('div');
+    const showVenueLabel = document.createElement('p');
+    showVenueLabel.classList = 'show-label';
+    showVenueLabel.innerText = 'VENUE';
+
+    console.log(show.venue)
+
+    const showVenueValue = document.createElement('p');
+    showVenueValue.classList = 'show-value';
+    showVenueValue.innerText = show.venue;
+
+    showVenueDiv.append(showVenueLabel);
+    showVenueDiv.append(showVenueValue);
+
+
+    const showLocationDiv = document.createElement('div');
+    const showLocationLabel = document.createElement('p');
+    showLocationLabel.classList = 'show-label';
+    showLocationLabel.innerText = 'LOCATION';
+
+    const showLocationValue = document.createElement('p');
+    showLocationValue.classList = 'show-value';
+    showLocationValue.innerText = show.location;
+
+    showLocationDiv.append(showVenueLabel);
+    showLocationDiv.append(showLocationValue);
+
+    const showButtonDiv = document.createElement('div');
+    const showButton = document.createElement('button');
+    showButton.innerText = 'BUY TICKETS';
+    showButton.type = 'submit';
+    showButtonDiv.append(showButton);
+
+    showItemDiv.append(showDateDiv);
+    showItemDiv.append(showVenueDiv);
+    showItemDiv.append(showLocationDiv);
+    showItemDiv.append(showButtonDiv);
+
+    showItemDiv.addEventListener('click', () => {
+        resetSelectedState();
+        showItemDiv.classList.add('show-item__selected');        
+    })
+
+    return showItemDiv;
+}
+
+function resetSelectedState() {
+    const showsList = document.querySelectorAll('.show-item');
+    showsList.forEach((show) => {
+        show.classList.remove('show-item__selected');
+    });
+}
+
+buildShowsSection();
+
+printShowTimes();
---
+++ scripts/index-page.js
@@ -0,0 +1,137 @@
+// dom elements
+let commentsForm = document.getElementsByClassName("comments__form");
+let nameField = document.getElementsByClassName("name__input");
+let commentField = document.getElementsByClassName("comment__input");
+
+// You must have an array in JavaScript with 3 default comment objects to start. Comments must have a name, a timestamp, and the comment text.
+let commentsArray = [
+    {
+        name:"Victor Pinto",
+        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.", 
+        timestamp:"11/02/2023"
+    },
+    {
+        name:"Christina Cabrera",
+        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+        timestamp:"10/28/2023"
+    },
+    {
+        name:"Isaac Tadesse",
+        comment: "I can't stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+        timestamp:"10/20/2023"
+    }
+]
+// TODO: You must have a function that takes in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.
+// TODO: All dynamic HTML should be added to DOM via DOM Methods for individual elements. Avoid bulk assigning stringified HTML using innerHTML
+function addComment(newComment) {
+    // Pushes a new comment object to an array of comments
+    commentsArray.push(newComment); 
+
+    // Re-renders to the page all comments from the comment array
+    printComments();
+    resetInputFields();
+}
+
+// Clears the input fields after submitting a new comment
+function resetInputFields() {
+    nameField[0].value = '';
+    commentField[0].value = '';
+}
+
+function createCommentDOMElement(comment) {
+    const commentCardContainerDiv = document.createElement('div');
+    commentCardContainerDiv.classList = "comment-card__container";
+
+    const avatarPlaceHolderDiv = document.createElement('div');
+    avatarPlaceHolderDiv.classList = "avatar-placeholder";
+
+    const commentCardTextDiv = document.createElement('div');
+    commentCardTextDiv.classList = "comment-card__text";
+
+    const commentNameTimestampDiv = document.createElement('div');
+    commentNameTimestampDiv.classList = "name__timestamp";
+
+    const commentName = document.createElement('h4');
+    const commentTimestamp = document.createElement('p');
+
+    const commentTextDiv = document.createElement('div');
+    commentTextDiv.classList = 'comment-text';
+
+    const commentText = document.createElement('p');
+    commentText.innerText = comment.comment;
+
+    commentCardContainerDiv.append(avatarPlaceHolderDiv);
+    commentCardContainerDiv.append(commentCardTextDiv);
+    commentCardTextDiv.append(commentNameTimestampDiv);
+    commentNameTimestampDiv.append(commentName);
+    commentNameTimestampDiv.append(commentTimestamp);
+    commentCardTextDiv.append(commentTextDiv);
+    commentTextDiv.append(commentText);
+
+    commentName.innerText = comment.name;
+    commentTimestamp.innerText = timeSince(new Date(comment.timestamp));
+
+    return commentCardContainerDiv;
+}
+
+// You must use an HTML Form with the following functionality:
+// That submits using the addEventListener
+commentsForm[0].addEventListener("submit", (event) => {
+    // Prevents the page from reloading when submitting a new comment
+    event.preventDefault();
+    if (nameField[0].value.length < 1 || commentField[0].value.length < 1) {
+        alert("please fill out all required fields");       
+    } else {
+        // Constructs a new comment object
+        let newComment= {
+            name: nameField[0].value,
+            comment: commentField[0].value,
+            timestamp: Date.now()
+        }
+        addComment(newComment);
+    }
+});
+
+function printComments() {
+    let commentsContainer = document.getElementsByClassName("comments-container");
+
+    // Clears all comments from the page
+    commentsContainer[0].innerHTML = '';
+
+    commentsArray.reverse();
+    for (let i = 0; i < commentsArray.length; i++){
+        const newCommentContainer = createCommentDOMElement(commentsArray[i]);
+        commentsContainer[0].append(newCommentContainer);
+    }
+    commentsArray.reverse();
+}
+
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+  
+    var interval = seconds / 31536000;
+  
+    if (interval > 1) {
+      return Math.floor(interval) + " years ago";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months ago";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days ago";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours ago";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+  }
+
+printComments();
